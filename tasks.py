2b1caaa97f45f7cb84186bfdab8059402fbb88d0
Pavel Katsev <pkatsev@gmail.com>
2015-01-16 22:15:46 -0500

    ref into luigi

---
+++ tasks.py
@@ -0,0 +1,79 @@
+import re
+import os
+from functools import partial
+import luigi
+import requests
+
+CFG = luigi.configuration.get_config()
+conf = partial(CFG.get, 'lastfm')
+conf_int = partial(CFG.getint, 'lastfm')
+
+API_KEY = conf('api_key')
+API_URL = conf('api_url', 'http://ws.audioscrobbler.com/2.0/')
+FORMAT = 'json'
+RAW_DATA_DIR = conf('raw_data_dir', 'data.raw')
+CHUNK_SIZE = conf_int('stream_chunk_bytes', pow(2, 13))
+
+SESH = requests.Session()
+SESH.params.update({'api_key': API_KEY, 'format': FORMAT})
+
+
+def api_get(**kwargs):
+    return SESH.get(API_URL, stream=True, params=kwargs)
+
+
+class LoadTopArtists(luigi.Task):
+    """Loads top artists using Last.fm API"""
+    api_method = 'chart.getTopArtists'
+    limit = conf_int('top_artist_limit', 1000)
+
+    def output(self):
+        filename = 'top_artists.{ext}'.format(ext=FORMAT)
+        return luigi.LocalTarget(os.path.join(RAW_DATA_DIR, filename))
+
+    def run(self):
+        # send the request
+        r = api_get(method=self.api_method, limit=self.limit)
+        r.raise_for_status()
+        # stream the response content into a file
+        with self.output().open('w') as output:
+            for chunk in r.iter_content(chunk_size=CHUNK_SIZE):
+                output.write(chunk)
+
+
+class LoadTopTags(luigi.Task):
+    """Loads top tags for the top artists using Last.fm API"""
+    api_method = 'artist.getTopTags'
+    mbid = luigi.Parameter()
+
+    def output(self):
+        filename = 'tags_{mbid}.{ext}'.format(mbid=self.mbid, ext=FORMAT)
+        return luigi.LocalTarget(os.path.join(RAW_DATA_DIR, filename))
+
+    def run(self):
+        # send the request
+        r = api_get(method=self.api_method)
+        r.raise_for_status()
+        # stream the response content into a file
+        with self.output().open('w') as output:
+            for chunk in r.iter_content(chunk_size=CHUNK_SIZE):
+                output.write(chunk)
+
+
+class LoadTopTagsForArtists(luigi.Task):
+    """Loads top tags for the top artists using Last.fm API"""
+    mbid_pattern = "{h}{{8}}-{h}{{4}}-{h}{{4}}-{h}{{4}}-{h}{{12}}".format(
+        h="[A-Fa-f0-9]")
+
+    def requires(self):
+        return LoadTopArtists()
+
+    def output(self):
+        with self.input().open('r') as artists:
+            mbids = re.findall(self.mbid_pattern, artists.read())
+        for mbid in mbids:
+            yield LoadTopTags(mbid=mbid)
+
+
+if __name__ == '__main__':
+    luigi.run()
